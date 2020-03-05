9dc380851518fd499734d83274cfee780d73fa3f
vu004 <49575760+vu004@users.noreply.github.com>
2020-03-04 22:48:47 -0500

    lab 4 commit
    making changes to lab 4

---
+++ lab_4/main.js
@@ -0,0 +1,40 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'google.png') {
+        myImage.setAttribute ('src', 'google2.png');
+    } else {
+        myImage.setAttribute ('src', 'google.png')
+    }
+
+
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla is cool, ' + myName;
+
+    
+
+    }
+    
+}
+if(!localStorage.getItem('name')) {
+    setUserName();
+
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
